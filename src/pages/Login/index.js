9ac67aaf577a61e743fecd322580086fb1f6b9e8
fanay <1909509136@qq.com>
2026-06-24 09:09:30 +0800

    '登录界面及表单校验'

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,38 @@
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
+
 const Login = () => {
-  return <div>this is Login</div>
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form validateTrigger='onChange'>
+          <Form.Item
+            name="mobile"
+            /* 多条验证规则，串行校验 */
+            rules={[
+              { required: true, message: '请输入手机号' },
+              , { pattern: /^1[3-9]\d{9}$/, message: '请输入正确的手机号' }
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[{ required: true, message: '请输入验证码' }]}>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div >
+  )
 }
+
 export default Login
