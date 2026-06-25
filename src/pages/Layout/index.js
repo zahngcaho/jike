6eaa0af8f83a3ac82c9b857d465751cdd1f1d308
fanay <1909509136@qq.com>
2026-06-25 08:10:37 +0800

    请求拦截器 token 注入

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,4 +1,9 @@
+import requset from '@/utils/request'
+import { useEffect } from 'react'
 const Layout = () => {
+  useEffect(() => {
+    requset.get('/user/profile')
+  })
   return <div>this is layout</div>
 }
 export default Layout
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 // axios的封装处理
 import axios from 'axios'
+import { getToken } from '@/utils/token'
 
 // 1.根域名配置 
 // 2.timeout config
@@ -11,21 +12,28 @@ const request = axios.create({
 })
 
 // 添加请求拦截器
-request.interceptors.request.use((config)=> {
-    return config
-  }, (error)=> {
-    return Promise.reject(error)
+request.interceptors.request.use((config) => {
+  // 在config中添加token
+  // 1.获取token
+  const token = getToken()
+  if(token){
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  // 2.按照后端的要求拼接成token
+  return config
+}, (error) => {
+  return Promise.reject(error)
 })
 
 // 添加响应拦截器
-request.interceptors.response.use((response)=> {
-    // 2xx 范围内的状态码都会触发该函数。
-    // 对响应数据做点什么
-    return response.data
-  }, (error)=> {
-    // 超出 2xx 范围的状态码都会触发该函数。
-    // 对响应错误做点什么
-    return Promise.reject(error)
+request.interceptors.response.use((response) => {
+  // 2xx 范围内的状态码都会触发该函数。
+  // 对响应数据做点什么
+  return response.data
+}, (error) => {
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // 对响应错误做点什么
+  return Promise.reject(error)
 })
 
 export default request
