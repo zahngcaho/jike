c39555a389aa23595847cdb92463f4134df2dd9f
fanay <1909509136@qq.com>
2026-06-25 08:30:56 +0800

    路由token拦截

---
+++ src/components/AuthRoute.js
@@ -0,0 +1,15 @@
+// 封装高阶组件
+// 有token正确跳转 无token跳转登录界面
+import { getToken } from '@/utils'
+import { Navigate } from 'react-router-dom'
+
+const AuthRoute = ({ children }) => {
+  const isToken = getToken()
+  if (isToken) {
+    return <>{children}</>
+  } else {
+    return <Navigate to="/login" replace />
+  }
+}
+
+export default AuthRoute
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
 import { createBrowserRouter } from "react-router-dom"
+import AuthRoute from '@/components/AuthRoute'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: Layout
+    element: <AuthRoute> <Layout /> </AuthRoute>
   },
   {
     path: "/login",
