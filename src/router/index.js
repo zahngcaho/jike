da29be638e62a784cc1aad266c0474d8511027cb
fanay <1909509136@qq.com>
2026-06-25 08:44:16 +0800

    Layout二级路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,28 @@ import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
 import { createBrowserRouter } from "react-router-dom"
 import AuthRoute from '@/components/AuthRoute'
+import Home from '@/pages/Home'
+import Article from '@/pages/Article'
+import Publish from '@/pages/Publish'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <AuthRoute> <Layout /> </AuthRoute>
+    element: <AuthRoute> <Layout /> </AuthRoute>,
+    children: [
+      {
+        path: "/home",
+        Component: Home
+      },
+      {
+        path: "/article",
+        Component: Article
+      },
+      {
+        path: "/publish",
+        Component: Publish
+      }
+    ]
   },
   {
     path: "/login",
